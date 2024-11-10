fdc5c33e25b277e752c1d0e13c728e23e34a7b16
Kuppireddy Yeshitha <168415012+yeshitha-kuppireddy@users.noreply.github.com>
2024-11-10 14:49:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.py
@@ -0,0 +1,110 @@
+'''
+# node class:
+
+class Node:
+    def __init__(self,val):
+        self.next=None
+        self.data=val
+
+'''
+
+class Solution:
+    #Function to remove a loop in the linked list.
+    def removeLoop(self, head):
+        # code here
+        # remove the loop without losing any nodes
+        chead=head
+        seen={}
+        while chead:
+            if chead.next is None:
+                return chead
+            if chead.next in seen:
+                chead.next=None
+                break
+            else:
+                seen[chead]=1
+                chead=chead.next
+        return head
+
+
+#{ 
+ # Driver Code Starts
+# driver code:
+
+
+class Node:
+
+    def __init__(self, val):
+        self.next = None
+        self.data = val
+
+
+class linkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add(self, num):
+        if self.head is None:
+            self.head = Node(num)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(num)
+            self.tail = self.tail.next
+
+    def isLoop(self):
+        if self.head is None:
+            return False
+
+        fast = self.head.next
+        slow = self.head
+
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            fast = fast.next.next
+            slow = slow.next
+
+        return True
+
+    def loopHere(self, position):
+        if position == 0:
+            return
+
+        walk = self.head
+        for _ in range(1, position):
+            walk = walk.next
+        self.tail.next = walk
+
+    def length(self):
+        walk = self.head
+        ret = 0
+        while walk:
+            ret += 1
+            walk = walk.next
+        return ret
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        arr = tuple(int(x) for x in input().split())
+        pos = int(input())
+        n = len(arr)
+        ll = linkedList()
+        for i in arr:
+            ll.add(i)
+        ll.loopHere(pos)
+
+        Solution().removeLoop(ll.head)
+
+        if ll.isLoop() or ll.length() != n:
+            print("false")
+            continue
+        else:
+            print("true")
+        print("~")
+
+# } Driver Code Ends
